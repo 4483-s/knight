81be36a5407161e888c6f0401801ba2efb919eb6
Hermann <188955756+4483-s@users.noreply.github.com>
2025-09-07 20:35:41 +0800

    Finish

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
 function knightMoves(start, end) {
-  const startNode = new Squre(...start);
-  const q = [];
-  q.push(startNode);
+  if (!inputIsValid(start, end)) throw new Error("Invalid input, exiting...");
+  const q = [new Square(...start)];
   const record = [];
   let found;
+  let count = 0;
   while (q.length && !found) {
-    const current = q[0];
+    const current = q[count];
     const traversed = isVisited(current, record);
-    console.log(traversed);
     if (current.x === end[0] && current.y === end[1]) {
       found = true;
-      getOutputFromTarget(current);
+      return getOutputFromTarget(current);
     } else if (!traversed) {
-      const adj = findAdj(current);
-      q.push(...adj);
-      record.push(...adj);
+      q.push(...findAdj(current));
+      record.push(current);
     }
-    q.shift();
+    count++;
   }
 }
-function getOutputFromTarget(squre) {
-  let temp = squre;
+function inputIsValid(start, end) {
+  return [...start, ...end].every((v) => v >= 0 && v < 8);
+}
+function getOutputFromTarget(square) {
+  let temp = square;
   const arr = [];
   while (temp) {
-    arr.push([temp.x, temp.y]);
+    arr.unshift([temp.x, temp.y]);
     temp = temp.parent;
   }
   console.log(`You made it in ${arr.length - 1} moves!  Here's your path:`);
-  for (let i = arr.length - 1; i >= 0; i--) {
-    console.log(arr[i]);
-  }
+  for (let i = 0; i < arr.length; i++) console.log(arr[i]);
+  return arr;
 }
 function isVisited(obj, arr) {
-  for (const v of arr) {
-    if (v.x === obj.x && v.y === obj.y) return true;
-  }
+  for (const v of arr) if (v.x === obj.x && v.y === obj.y) return true;
   return false;
 }
-//
-
 function findAdj(parent) {
   const { x, y } = parent;
   const temp = [];
@@ -54,11 +50,11 @@ function findAdj(parent) {
   let result = [];
   temp.forEach((v) => {
     if (v[0] >= 0 && v[1] >= 0 && v[0] < 8 && v[1] < 8)
-      result.push(new Squre(v[0], v[1], parent));
+      result.push(new Square(v[0], v[1], parent));
   });
   return result;
 }
-class Squre {
+class Square {
   constructor(x, y, parent = null) {
     this.x = x;
     this.y = y;
